ce39812e235836431be0a8231de8aa6706d89107
fernando2523 <74076122+fernando2523@users.noreply.github.com>
2023-12-15 23:48:34 +0700

    new mobile

--- pages/DetailProduct.js
+++ pages/DetailProduct.js
@@ -99,22 +99,24 @@ export default function AddPo({ route, navigation }) {
         automaticallyAdjustKeyboardInsets={true}
         className="h-full"
       >
-        <View className="aspect-square rounded-b-md -mb-5 -ml-8">
-          <Image
-            source={{
-              uri: `http://139.180.130.182:4000/assets/img/${dataDetails.item.images}`,
-            }}
-            style={{
-              width: "100%",
-              height: "100%",
-              resizeMode: "contain",
-            }}
-          />
+        <View className="-mb-5">
+          <View className="aspect-square">
+            <Image
+              source={{
+                uri: `http://139.180.130.182:4000/assets/img/${dataDetails.item.images}`,
+              }}
+              style={{
+                width: "100%",
+                height: "100%",
+                resizeMode: "contain",
+              }}
+            />
+          </View>
         </View>
 
-        <View className="-mt-5 bg-black w-[50%] mx-auto rounded-3xl p-2 flex items-center -mb-5 z-10 shadow-sm">
+        <View className="-mt-5 -mb-5 z-50 bg-green-600 w-[50%] mx-auto rounded-3xl p-2 flex items-center">
           <Text className="font-bold text-xl text-white">
-            Available Stock {" "}
+            Available Stock{" : "}
             {Numbering.format(
               dataDetails.item.total_stok === null
                 ? 0
@@ -123,8 +125,8 @@ export default function AddPo({ route, navigation }) {
           </Text>
         </View>
 
-        <View className="border border-gray-300 border-b-0 rounded-t-3xl bg-white">
-          <View className="mx-4 flex-row items-center h-auto border-b border-gray-300 py-2">
+        <View className="border border-gray-300 border-b-0 pt-3 rounded-t-3xl bg-white">
+          <View className="mx-4 flex-row items-center h-auto border-b border-gray-300 py-2  ">
             <View className="basis-3/4">
               <Text
                 numberOfLines={2}
@@ -132,7 +134,7 @@ export default function AddPo({ route, navigation }) {
                 className={`${dataDetails.item.produk == "null"
                   ? "text-red-500"
                   : "text-[#2e2e2e]"
-                  } font-bold text-xl mt-4`}
+                  } font-bold text-xl `}
               >
                 {dataDetails.item.produk == "null"
                   ? "Belum ada nama"
@@ -148,7 +150,9 @@ export default function AddPo({ route, navigation }) {
 
             <Text className="basis-1/4 pt-1 font-bold text-md text-right">
               {Rupiah.format(dataDetails.item.harga_jual)}{" "}
-              <Text className="text-xs  text-red-500">400Gr</Text>
+              <Text className="text-xs  text-red-500">
+                {dataDetails.item.berat_produk}Gr
+              </Text>
             </Text>
           </View>
 
@@ -158,70 +162,70 @@ export default function AddPo({ route, navigation }) {
                 {/* <Text className="text-black text-xs">
                 Supplier
               </Text> */}
-                <Text className="text-black text-xs">
-                  Cost Kurs
-                </Text>
-                <Text className="text-black text-xs">
-                  Cost
-                </Text>
-                <Text className="text-black text-xs">
-                  Overhead
-                </Text>
+                <Text className="text-black text-xs">Cost Kurs</Text>
+                <Text className="text-black text-xs">Cost</Text>
+                <Text className="text-black text-xs">Overhead</Text>
                 {/* <Text className="text-black text-xs">
                 Selling Price
               </Text> */}
-                <Text className="text-black text-xs">
-                  Profit
-                </Text>
+                <Text className="text-black text-xs">Profit</Text>
               </View>
               <View className="basis-1/2 ">
                 {/* <Text className="font-bold text-xs text-right">{dataDetails.item.supplier}</Text> */}
-                <Text className="font-bold text-xs text-right">{dataDetails.item.modal_asing}</Text>
-                <Text className="font-bold text-xs text-right">{Rupiah.format(dataDetails.item.modal_rp)}</Text>
-                <Text className="font-bold text-xs text-right">{Rupiah.format(dataDetails.item.modal_overhead_rp)}</Text>
+                <Text className="font-bold text-xs text-right">
+                  {dataDetails.item.modal_asing}
+                </Text>
+                <Text className="font-bold text-xs text-right">
+                  {Rupiah.format(dataDetails.item.modal_rp)}
+                </Text>
+                <Text className="font-bold text-xs text-right">
+                  {Rupiah.format(dataDetails.item.modal_overhead_rp)}
+                </Text>
                 {/* <Text className="font-bold text-xs text-right">{Rupiah.format(dataDetails.item.harga_jual)}</Text> */}
-                <Text className="font-bold text-xs text-right">{Rupiah.format(dataDetails.item.profit)}</Text>
+                <Text className="font-bold text-xs text-right">
+                  {Rupiah.format(dataDetails.item.profit)}
+                </Text>
               </View>
             </View>
 
             <View className="basis-1/2 flex flex-row items-start h-auto mx-4 px-0">
               <View className="basis-1/2">
-                <Text className="text-black text-xs">
-                  Kurs
-                </Text>
-                <Text className="text-black text-xs">
-                  Overhead
-                </Text>
-                <Text className="text-black text-xs">
-                  Margin
-                </Text>
+                <Text className="text-black text-xs">KURS</Text>
+                <Text className="text-black text-xs">OVERHEAD</Text>
+                <Text className="text-black text-xs">MARGIN</Text>
               </View>
-              <View className="basis-1/2 px-4">
-                <Text className="font-bold text-xs text-right">{Rupiah.format(dataDetails.item.kurs)}</Text>
-                <Text className="font-bold text-xs text-right">{Rupiah.format(dataDetails.item.overhead)}</Text>
-                <Text className="font-bold text-xs text-right">{dataDetails.item.margin}%</Text>
+              <View className="basis-1/2 px-3">
+                <Text className="font-bold text-xs text-right">
+                  {Rupiah.format(dataDetails.item.kurs)}
+                </Text>
+                <Text className="font-bold text-xs text-right">
+                  {Rupiah.format(dataDetails.item.overhead)}
+                </Text>
+                <Text className="font-bold text-xs text-right">
+                  {dataDetails.item.margin}%
+                </Text>
               </View>
             </View>
           </View>
 
-          <View className="flex flex-row px-0 mr-4 mx-4 h-[35px] justify-center border-b border-gray-300">
-            <View className="basis-2/5 h-[40px] m-1 rounded-full shadow-sm">
-              <Text className="font-bold text-lg text-black text-center mt-0 shadow-md">
-                Orders {" "}
+          <View className="flex flex-row px-0 mr-4 mx-4 mt-2 h-[35px] justify-center">
+            <View className="basis-2/5 h-[40px] m-1 bg-black rounded-full shadow-sm ">
+              <Text className="font-bold text-xl text-white text-center mt-2">
+                Orders{" : "}
                 {Numbering.format(
-                  dataDetails.item.total_stok === null
+                  dataDetails.item.total_reserved === null
                     ? 0
-                    : dataDetails.item.total_stok
+                    : dataDetails.item.total_reserved
                 )}
               </Text>
             </View>
-            <View className="basis-2/5 h-[40px] m-1 rounded-full shadow-sm">
-              <Text className="font-bold text-lg text-red-600 text-center mt-0  shadow-md">
-                Unfilled {" "}
+            <View className="basis-2/5 h-[40px] m-1 bg-red-600 rounded-full shadow-sm">
+              <Text className="font-bold text-xl text-white text-center mt-2">
+                Unfulfilled{" : "}
                 {Numbering.format(
-                  dataDetails.item.total_stok === null
+                  dataDetails.item.total_permintaan === null
                     ? 0
-                    : dataDetails.item.total_stok
+                    : dataDetails.item.total_permintaan
                 )}
               </Text>
             </View>
@@ -229,11 +233,7 @@ export default function AddPo({ route, navigation }) {
 
           <View className="flex flex-row px-0 mr-4 mx-4 h-[35px]">
             <View className="basis-full h-[40px] m-1 rounded-full shadow-sm items-center">
-              <Ionicons
-                name="create-outline"
-                size={40}
-                color="black"
-              />
+              <Ionicons name="create-outline" size={40} color="black" />
             </View>
           </View>
         </View>
@@ -244,27 +244,96 @@ export default function AddPo({ route, navigation }) {
           visible={modalVisible}
         >
           <View style={styles.centeredView}>
-            <View style={styles.modalView} className=" bg-cyan-300 rounded-3xl border-t border-gray-200 shadow-md">
+            <View className="w-full h-[70%] mt-4">
+              <Text className="text-center text-xl -mt-2 font-bold mb-2">ADD VARIATIONS</Text>
+            </View>
+            <View style={styles.modalView}
+              className=" bg-cyan-300 rounded-3xl border-t border-gray-200 shadow-md">
               <View className="w-full h-[70%] p-4">
-                <Text className="text-center text-xl -mt-2 font-bold mb-2">VARIATIONS</Text>
+                <Text className="text-center text-xl -mt-2 font-bold mb-2">
+                  VARIATIONS
+                </Text>
                 {data_variasi.length < 1 ? (
                   <View className="flex-1 items-center justify-center">
                     <Text className="basis-1/12 text-center">
-                      There are no variations yet, please add variations and stock
+                      There are no variations yet, please add variations and
+                      stock
                     </Text>
                   </View>
                 ) : (
                   <>
-                    <View className="flex-row justify-center items-center rounded-md h-10 mb-2 px-2 border-b border-t border-gray-300">
+                    <View className="bg-white p-2 shadow-sm mt-4">
+                      <View className="flex flex-row border-b border-gray-200 pb-2 mt-1 ml-1">
+                        <Text className="basis-1/2 text-left font-medium">VARIATION</Text>
+                        <Text className="basis-1/2 text-right font-medium text-red-600">Change</Text>
+                      </View>
+
+                      <View className="flex flex-row border-b border-gray-200 pb-2 mb-2 ml-1">
+                        <View className="basis-full flex flex-row bg-cy">
+                          <View className="basis-grow text-left p-3 mt-2  border rounded-md border-red-600 mr-2">
+                            <Text className="font-medium  text-red-600">Navy</Text>
+                          </View>
+                          <View className="basis-grow text-left p-3 mt-2  border rounded-md border-red-600 mr-2">
+                            <Text className="font-medium  text-red-600">Black</Text>
+                          </View>
+                          <View className="basis-grow text-left p-3 mt-2  border rounded-md border-red-600 mr-2">
+                            <Text className="font-medium">+ Add</Text>
+                          </View>
+                        </View>
+                      </View>
+                    </View>
+
+                    <View className="bg-white p-2 shadow-sm mt-4">
+                      <View className="flex flex-row border-b border-gray-200 pb-2 mt-1 ml-1">
+                        <Text className="basis-1/2 text-left font-medium">SIZE</Text>
+                        <Text className="basis-1/2 text-right font-medium text-red-600">Change</Text>
+                      </View>
+
+                      <View className="flex flex-row border-b border-gray-200 pb-2 mb-2 ml-1">
+                        <View className="basis-full flex flex-row bg-cy">
+                          <View className="basis-grow text-left p-3 mt-2  border rounded-md border-red-600 mr-2">
+                            <Text className="font-medium  text-red-600">M</Text>
+                          </View>
+                          <View className="basis-grow text-left p-3 mt-2  border rounded-md border-red-600 mr-2">
+                            <Text className="font-medium  text-red-600">L</Text>
+                          </View>
+                          <View className="basis-grow text-left p-3 mt-2  border rounded-md border-red-600 mr-2">
+                            <Text className="font-medium  text-red-600">XL</Text>
+                          </View>
+                          <View className="basis-grow text-left p-3 mt-2  border rounded-md border-red-600 mr-2">
+                            <Text className="font-medium">+ Add</Text>
+                          </View>
+                        </View>
+                      </View>
+                    </View>
+
+                    {/* <View className="flex-row justify-center items-center rounded-md h-10 mb-2 px-2 border-b border-t border-gray-300">
                       <Text className="basis-1/12 text-center text-xs">NO.</Text>
                       <Text className="basis-2/12 text-center text-xs">COLOR</Text>
                       <Text className="basis-2/12 text-center text-xs">SIZE</Text>
                       <Text className="basis-2/12 text-center text-xs">AVAILABLE</Text>
+                    <View className="flex-row justify-center items-center rounded-md h-10 mb-2 px-2 border-b border-t border-gray-300">
+                      <Text className="basis-1/12 text-center text-xs">
+                        NO.
+                      </Text>
+                      <Text className="basis-2/12 text-center text-xs">
+                        COLOR
+                      </Text>
+                      <Text className="basis-2/12 text-center text-xs">
+                        SIZE
+                      </Text>
+                      <Text className="basis-2/12 text-center text-xs">
+                        AVAILABLE
+                      </Text>
                       <Text className="basis-2/12 text-center text-xs">
                         ORDER
                       </Text>
-                      <Text className="basis-2/12 text-center text-xs">UNFILLED</Text>
-                      <Text className="basis-1/12 text-center text-xs">ACT</Text>
+                      <Text className="basis-2/12 text-center text-xs">
+                        UNFILLED
+                      </Text>
+                      <Text className="basis-1/12 text-center text-xs">
+                        ACT
+                      </Text>
                     </View>
                     <View
                       style={{
@@ -279,7 +348,7 @@ export default function AddPo({ route, navigation }) {
                           itemProduct(item, index)
                         }
                       />
-                    </View>
+                    </View> */}
                   </>
                 )}
               </View>
